7857e304c94da7191c00887420b026b2588b80cb
admin <admin@gmail.com>
2022-06-17 13:34:40 +0430

    darhale takmile booklist

--- ketabkhone/ketabkhone/settings.py
+++ ketabkhone/ketabkhone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'ketabkhone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,12 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
-import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    
--- ketabkhone/ketabkhone/views.py
+++ ketabkhone/ketabkhone/views.py
@@ -1,2 +1,16 @@
-def home():
-    pass
+from django.shortcuts import render
+from book.models import Book
+
+def bookdetail(request):
+    return render(request, 'bookdetail.html',)
+
+def bookliste(request):
+    return render(request, 'bookliste.html')
+
+def btest(request):
+    return render(request, 'btest.html')
+
+
+def contest(request):
+    context = {'book': Book.objects.all() , 'imgs':Book.objects.cover()}
+    return render(request, 'contest.html',context)
